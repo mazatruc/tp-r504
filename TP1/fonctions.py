276da5dfe3348022bfccb0a0abcff4d81f385822
mazatruc <mdelacourt2003@gmail.com>
2023-09-07 16:51:55 +0200

    TP1-V1.4

--- TP1/fonctions.py
+++ TP1/fonctions.py
@@ -1,26 +1,37 @@
-def puissance(a,b):
+def puissance2(a,b):
 	if not type(a) is int :
 		if not type(b) is int:
 			raise TypeError("only integers are allowed")
+#conditions des cas 0 :
 
 	if a == 0 and b == 0:
 		return 1
 
-	if b == 0 :
-		if a <= 0:
+	if b == 0:
+		if a <= -1:
 			return -1
 		else:
 			return 1
 
-	rep = abs(-a)
-	if b <= 0:
-		c = -1
+	reponse = abs(-a)
+
+#conditions des puissances négatives ou positives :
+
+	if b <= 0: #si b est négatif le pas est de -1 et la boucle commence à b positif
+		pas = -1
 		mn = abs(b)
-	else:
-		c = 1
+	else: #si b est positif le pas est de 1 et la boucle commence à 1
+		pas = 1
 		mn = 1
 
-	for cheh in range (mn,b,c):
-		rep = rep*a
+	for cheh in range (mn,b,pas):
+		reponse = reponse*a
 
-	return rep
+	return reponse
+def puissance(x,n):
+	sortie = 1
+	if x == 0 and n != 0:
+		return 0
+	for cheh in range(n):
+		sortie = sortie*x
+	return sortie
