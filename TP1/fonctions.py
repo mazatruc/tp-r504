36e6da659a748d5aa1e909c87bc946f708077218
mazatruc <mdelacourt2003@gmail.com>
2023-09-07 16:24:21 +0200

    TP1-V1.2

--- TP1/fonctions.py
+++ TP1/fonctions.py
@@ -3,11 +3,24 @@ def puissance(a,b):
 		if not type(b) is int:
 			raise TypeError("only integers are allowed")
 
-	if a == 0:
+	if a == 0 and b == 0:
 		return 1
 
+	if b == 0 :
+		if a <= 0:
+			return -1
+		else:
+			return 1
+
 	rep = abs(-a)
-	for cheh in range (1,b):
+	if b <= 0:
+		c = -1
+		mn = abs(b)
+	else:
+		c = 1
+		mn = 1
+
+	for cheh in range (mn,b,c):
 		rep = rep*a
 
-	return abs(rep)
+	return rep
--- TP1/test.py
+++ TP1/test.py
@@ -6,8 +6,12 @@ def test_1():
 	assert f.puissance(2,2) == 4
 
 def test_2():
-	assert f.puissance((-2),3) == 8
-	assert f.puissance((-8),2) == 64
+	assert f.puissance(2,0) == 1
+	assert f.puissance(-2,0) == -1
 
 def test_3():
 	assert f.puissance(0,0) == 1
+
+def test_4():
+	assert f.puissance(0,3) == 0
+	assert f.puissance(0,-3) == 0
