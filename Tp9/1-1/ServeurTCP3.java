4c4a72eaa1bd501177b9f1e3e556b06384cdf969
user <user@Ratio.myguest.virtualbox.org>
2024-01-22 11:06:07 +0100

    V1.2

---
+++ Tp9/1-1/ServeurTCP3.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.net.*;
+
+public class ServeurTCP3
+{
+	public static void main( String[] args )
+	{
+		while (true){
+			try {
+				ServerSocket socketserver = new ServerSocket( 2016 );
+				System.out.println( "serveur en attente" );
+				Socket socket = socketserver.accept();
+				System.out.println( "Connection d'un client" );
+				DataInputStream dIn = new DataInputStream( socket.getInputStream() );
+				String rev = new StringBuilder(args[0]).reverse().toString();
+				System.out.println( "Message : " + dIn.readUTF() );
+	
+				socket.close();
+				socketserver.close();
+			}
+			catch( Exception ex )
+			{
+				System.out.println( "mother fuc" );
+				ex.printStackTrace();
+			}
+		}
+	}
+}
